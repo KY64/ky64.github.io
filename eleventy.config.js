9b224b1fb2f3297a96bda073dd6940509431d57e
KY64 <taurus99@protonmail.com>
2024-10-27 12:30:25 +0700

    refactor: split css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
-  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPassthroughCopy("src/css/style.css");
+  eleventyConfig.addPassthroughCopy("src/css/mobile.css");
   eleventyConfig.addNunjucksFilter(
     "date",
     (date) => new Date(date).toISOString().split("T")[0],
