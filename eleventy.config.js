6c49a5b7889cfddccb5463e4250432861f3092c0
KY64 <taurus99@protonmail.com>
2026-06-14 18:52:54 +0700

    chore: add link to header

--- eleventy.config.js
+++ eleventy.config.js
@@ -12,7 +12,7 @@ export default function (eleventyConfig) {
     (date) => new Date(date).toISOString().split("T")[0],
   );
 
-  const md = markdownIt();
+  const md = markdownIt({ html: true, linkify: true, typographer: true });
   const defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
     return self.renderToken(tokens, idx, options);
   };
@@ -21,6 +21,24 @@ export default function (eleventyConfig) {
     tokens[idx].attrSet("target", "_blank");
     tokens[idx].attrSet("rel", "noopener");
     return defaultRender(tokens, idx, options, env, self);
+  };
+
+  const slug = (text) => text.toLowerCase().replace(/[^\w]+/g, "-").replace(/(^-|-$)/g, "");
+  const defaultHeading = md.renderer.rules.heading_open || function(tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+  md.renderer.rules.heading_open = function(tokens, idx, options, env, self) {
+    const title = tokens[idx + 1].children.reduce((acc, t) => acc + t.content, "");
+    const id = slug(title);
+    tokens[idx].attrSet("id", id);
+    return `${defaultHeading(tokens, idx, options, env, self)}<a href="#${id}">`;
+  };
+
+  md.renderer.rules.heading_close = function(tokens, idx, options, env, self) {
+    const tag = tokens[idx].tag;
+    const defaultClose = self.renderToken(tokens, idx, options);
+    return `</a>${defaultClose}`;
   };
 
   eleventyConfig.setLibrary("md", md);
