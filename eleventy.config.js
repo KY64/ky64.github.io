dcb7b259c1d395a70c81503e13821d8c4525691e
KY64 <taurus99@protonmail.com>
2026-06-14 11:21:20 +0700

    fix: open link in new tab

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,9 @@
 import CleanCSS from "clean-css";
+import markdownIt from "markdown-it";
 
 export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
-  eleventyConfig.addFilter("cssmin", (code) => 
+  eleventyConfig.addFilter("cssmin", (code) =>
     new CleanCSS({level: 2}).minify(code).styles);
   eleventyConfig.addPassthroughCopy("src/css/mobile.css");
   eleventyConfig.addPassthroughCopy("src/images");
@@ -10,4 +11,17 @@ export default function (eleventyConfig) {
     "date",
     (date) => new Date(date).toISOString().split("T")[0],
   );
+
+  const md = markdownIt();
+  const defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+  md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
+    tokens[idx].attrSet("target", "_blank");
+    tokens[idx].attrSet("rel", "noopener");
+    return defaultRender(tokens, idx, options, env, self);
+  };
+
+  eleventyConfig.setLibrary("md", md);
 }
